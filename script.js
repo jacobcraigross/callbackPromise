ba3bea5917921bd6426c5f8390ce922fe6c51a2f
Jake Ross <jakeross@Jakes-MacBook-Pro.local>
2017-03-28 09:42:08 -0500

    JQ 1.0

---
+++ script.js
@@ -0,0 +1,13 @@
+$("h1").css({
+  fontSize: "150px",
+  color: "#f3f3f3",
+  fontFamily: "helvetica"
+});
+
+$("body").css("background", "#ff4040");
+
+$("li").css({
+  fontSize: "60px",
+  fontFamily: "helvetica",
+  color: "#f3f3f3"
+});
